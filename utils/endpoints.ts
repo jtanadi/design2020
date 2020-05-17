9fbe29c71785d60e986121170b04ba61257d9511
Jesen Tanadi <mail@jesentanadi.com>
2020-05-17 18:37:40 -0400

    allow local dev

--- utils/endpoints.ts
+++ utils/endpoints.ts
@@ -1,2 +1,11 @@
-export const domain = "https://design2020-api.jtanadi.now.sh";
-export const api = domain + "/api";
+let domain: string;
+if (process.env.LOCAL_API === "true") {
+  domain = "http://localhost:3001";
+  console.log(`Endpoint is ${domain}`);
+} else {
+  domain = "https://design2020-api.jtanadi.now.sh";
+}
+
+const api = domain + "/api";
+
+export { domain, api };
